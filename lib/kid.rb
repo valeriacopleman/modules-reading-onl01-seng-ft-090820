b6e58cb4da9f0a4bc6dae2f654fca58ba6f069b9
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-25 03:03:03 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,19 +1,5 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid
-  
-  extend MetaDancing
-  
-  include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
-  
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-  
+class Kid 
 end
